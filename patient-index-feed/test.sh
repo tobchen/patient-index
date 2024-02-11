fcb321711bca76f9fed61db838736e5c616fa30d
Tobias Heukäufer <mail@tobchen.de>
2024-02-11 12:15:34 +0100

    Docker compose now builds the whole patient index system

--- patient-index-feed/test.sh
+++ patient-index-feed/test.sh
@@ -6,5 +6,4 @@ java -javaagent:../otel/opentelemetry-javaagent.jar \
     -Dspring.datasource.url=jdbc:postgresql://localhost:5442/postgres \
     -Dspring.datasource.username=postgres -Dspring.datasource.password=password \
     -Dspring.jpa.hibernate.ddl-auto=update \
-    -Dserver.port=8080 \
     -jar target/patient-index-feed-0.0.1-SNAPSHOT.jar
