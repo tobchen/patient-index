199b6e973f7bf158db891a2f83c24303340a70ec
Tobias Heukäufer <mail@tobchen.de>
2024-02-15 19:13:20 +0100

    Patient Identity Feed is now on HAPI 7

--- patient-index-feed/src/main/java/de/tobchen/health/patientindex/feed/model/entities/MessageEntity.java
+++ patient-index-feed/src/main/java/de/tobchen/health/patientindex/feed/model/entities/MessageEntity.java
@@ -2,13 +2,13 @@ package de.tobchen.health.patientindex.feed.model.entities;
 
 import java.time.Instant;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 import org.springframework.lang.Nullable;
 
