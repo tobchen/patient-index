b11b032484179821a8c963f48d9f41d82accb1f2
Tobias Heukäufer <mail@tobchen.de>
2024-04-27 20:01:28 +0200

    Moved patient services to patient service.

--- patient-index-main/src/main/java/de/tobchen/health/patientindex/main/providers/PatientProvider.java
+++ patient-index-main/src/main/java/de/tobchen/health/patientindex/main/providers/PatientProvider.java
@@ -1,14 +1,9 @@
 package de.tobchen.health.patientindex.main.providers;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
-
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.r5.model.IdType;
-import org.hl7.fhir.r5.model.Meta;
 import org.hl7.fhir.r5.model.OperationOutcome;
 import org.hl7.fhir.r5.model.Parameters;
 import org.hl7.fhir.r5.model.Parameters.ParametersParameterComponent;
@@ -17,20 +12,12 @@ import org.hl7.fhir.r5.model.Reference;
 import org.hl7.fhir.r5.model.OperationOutcome.IssueSeverity;
 import org.hl7.fhir.r5.model.OperationOutcome.IssueType;
 import org.hl7.fhir.r5.model.OperationOutcome.OperationOutcomeIssueComponent;
-import org.hl7.fhir.r5.model.Patient.LinkType;
-import org.jooq.DSLContext;
-import org.jooq.JSONB;
-import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import ca.uhn.fhir.rest.annotation.Create;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Operation;
@@ -44,10 +31,7 @@ import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.param.TokenParam;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
-import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
-
-import static de.tobchen.health.patientindex.main.jooq.public_.Tables.*;
-import de.tobchen.health.patientindex.main.jooq.public_.tables.records.PatientRecord;
+import de.tobchen.health.patientindex.main.services.PatientService;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Tracer;
@@ -59,14 +43,12 @@ public class PatientProvider implements IResourceProvider
 
     private final Tracer tracer;
 
-    private final DSLContext dsl;
+    private final PatientService service;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public PatientProvider(OpenTelemetry openTelemetry, DSLContext dsl)
+    public PatientProvider(OpenTelemetry openTelemetry, PatientService service)
     {
         this.tracer = openTelemetry.getTracer(PatientProvider.class.getName());
-        this.dsl = dsl;
+        this.service = service;
     }
 
     @Override
@@ -82,7 +64,7 @@ public class PatientProvider implements IResourceProvider
 
         try (var scope = span.makeCurrent())
         {
-            var outcome = insertOrUpdatePatient(null, patient);
+            var outcome = service.createOrUpdate(patient);
 
             span.setAttribute("audit.action", "create");
             span.setAttribute("audit.patient", outcome.getId().getIdPart());
@@ -116,7 +98,7 @@ public class PatientProvider implements IResourceProvider
                 throw new InvalidRequestException("Id is missing id part");
             }
 
-            var outcome = insertOrUpdatePatient(idPart, patient);
+            var outcome = service.createOrUpdate(patient);
 
             span.setAttribute("audit.action", outcome.getCreated().booleanValue() ? "create" : "update");
             span.setAttribute("audit.patient", outcome.getId().getIdPart());
@@ -137,31 +119,21 @@ public class PatientProvider implements IResourceProvider
     }
 
     @Read
-    public @Nullable Patient read(@IdParam IIdType resourceId) throws JsonProcessingException
+    public @Nullable Patient read(@IdParam IIdType id) throws JsonProcessingException
     {
         var span = tracer.spanBuilder("PatientProvider.read").startSpan();
 
         try (var scope = span.makeCurrent())
         {
-            Patient resource;
+            Patient patient = service.get(id);
 
-            var record = dsl.select(PATIENT)
-                .from(PATIENT)
-                .where(PATIENT.ID.equal(resourceId.getIdPart()))
-                .fetchAny();
-            if (record != null)
+            if (patient != null)
             {
-                resource = resourceFromRecord(record.value1());
-
                 span.setAttribute("audit.action", "read");
-                span.setAttribute("audit.patient", resource.getIdPart());
-            }
-            else
-            {
-                resource = null;
+                span.setAttribute("audit.patient", patient.getIdPart());
             }
 
-            return resource;
+            return patient;
         }
         catch (Throwable t)
         {
@@ -183,27 +155,18 @@ public class PatientProvider implements IResourceProvider
 
         try (var scope = span.makeCurrent())
         {
-            var result = new ArrayList<Patient>();
-            var patientIds = new ArrayList<String>();
-
-            var identifier = new IdentifierRecord(resourceIdentifier.getSystem(), resourceIdentifier.getValue());
-            var identifierJson = objectMapper.writeValueAsString(new IdentifierRecord[] { identifier });
-
-            var records = dsl.select(PATIENT)
-                .from(PATIENT)
-                .where(PATIENT.IDENTIFIERS.contains(JSONB.jsonb(identifierJson)))
-                .fetch();
-            for (var record : records)
+            var patients = service.findByIdentifier(resourceIdentifier.getSystem(), resourceIdentifier.getValue());
+            
+            var ids = new ArrayList<String>();
+            for (var patient : patients)
             {
-                var resource = resourceFromRecord(record.value1());
-                result.add(resource);
-                patientIds.add(resource.getIdPart());
+                ids.add(patient.getIdPart());
             }
 
             span.setAttribute("audit.action", "search");
-            span.setAttribute(AttributeKey.stringArrayKey("audit.patient"), patientIds);
+            span.setAttribute(AttributeKey.stringArrayKey("audit.patient"), ids);
 
-            return result;
+            return patients;
         }
         catch (Throwable t)
         {
@@ -225,66 +188,24 @@ public class PatientProvider implements IResourceProvider
 
         try (var scope = span.makeCurrent())
         {
-            var sourceIdType = sourceReference.getReferenceElement();
-            var targetIdType = targetReference.getReferenceElement();
+            var sourceId = sourceReference.getReferenceElement();
+            var targetId = targetReference.getReferenceElement();
 
-            if (sourceIdType == null || targetIdType == null)
+            if (sourceId == null || targetId == null)
             {
                 throw new InvalidRequestException("Needs source and target ids");
             }
-            else if (sourceIdType.hasBaseUrl() || targetIdType.hasBaseUrl())
+            else if (sourceId.hasBaseUrl() || targetId.hasBaseUrl())
             {
                 throw new InvalidRequestException("Cannot handle absolute references");
             }
-            else if (!"Patient".equals(sourceIdType.getResourceType())
-                || !"Patient".equals(targetIdType.getResourceType()))
+            else if (!"Patient".equals(sourceId.getResourceType())
+                || !"Patient".equals(targetId.getResourceType()))
             {
                 throw new InvalidRequestException("Source and target must be Patient");
             }
 
-            var sourceId = sourceIdType.getIdPart();
-            var targetId = targetIdType.getIdPart();
-
-            var record = dsl.transactionResult(trx -> {
-                var targetFetch = trx.dsl().select(PATIENT)
-                    .from(PATIENT)
-                    .where(PATIENT.ID.equal(targetId))
-                    .fetchAny();
-                if (targetFetch == null)
-                {
-                    throw new InvalidRequestException("Target does not exist");
-                }
-                
-                var targetRecord = targetFetch.value1();
-                if (targetRecord.getMergedInto() != null)
-                {
-                    throw new UnprocessableEntityException("Target is already merged");
-                }
-
-                var sourceFetch = trx.dsl().select(PATIENT.MERGED_INTO)
-                    .from(PATIENT)
-                    .where(PATIENT.ID.equal(sourceId))
-                    .fetchAny();
-                if (sourceFetch == null)
-                {
-                    throw new InvalidRequestException("Source does not exist");
-                }
-
-                if (sourceFetch.value1() != null)
-                {
-                    throw new UnprocessableEntityException("Source is already merged");
-                }
-
-                trx.dsl().update(PATIENT)
-                    .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
-                    .set(PATIENT.MERGED_INTO, targetId)
-                    .where(PATIENT.ID.equal(sourceId))
-                    .execute();
-
-                return targetRecord;
-            });
-
-            var resource = resourceFromRecord(record);
+            var mergeResult = service.merge(sourceId, targetId);
 
             var parameters = new Parameters()
                 .addParameter(new ParametersParameterComponent("input")
@@ -296,11 +217,11 @@ public class PatientProvider implements IResourceProvider
 
             parameters.addParameter().setName("outcome").setResource(
                 new OperationOutcome(new OperationOutcomeIssueComponent(IssueSeverity.SUCCESS, IssueType.SUCCESS)));
-            parameters.addParameter().setName("result").setResource(resource);
+            parameters.addParameter().setName("result").setResource(mergeResult.target());
 
             span.setAttribute("audit.action", "merge");
-            span.setAttribute("audit.patient.soure", sourceId);
-            span.setAttribute("audit.patient.target", targetId);
+            span.setAttribute("audit.patient.soure", sourceId.getIdPart());
+            span.setAttribute("audit.patient.target", targetId.getIdPart());
 
             return parameters;
         }
@@ -314,135 +235,4 @@ public class PatientProvider implements IResourceProvider
             span.end();
         }
     }
-
-    private MethodOutcome insertOrUpdatePatient(@Nullable String id, Patient patient)
-        throws JsonProcessingException
-    {
-        var identifierList = new ArrayList<IdentifierRecord>();
-
-        var patientIdentifiers = patient.getIdentifier();
-        if (patientIdentifiers != null)
-        {
-            for (var patientIdentifier : patientIdentifiers)
-            {
-                if (patientIdentifier != null)
-                {
-                    var system = patientIdentifier.getSystem();
-                    var value = patientIdentifier.getValue();
-                    if (system != null && value != null)
-                    {
-                        identifierList.add(new IdentifierRecord(system, value));
-                    }
-                }
-            }
-        }
-
-        var identifierJson = objectMapper.writeValueAsString(identifierList);
-
-        var result = dsl.transactionResult(trx -> {
-            var resourceId = id;
-
-            Boolean created;
-            PatientRecord patientRecord;
-
-            if (resourceId == null)
-            {
-                do
-                {
-                    resourceId = UUID.randomUUID().toString();
-                }
-                while (trx.dsl().selectOne().from(PATIENT)
-                    .where(PATIENT.ID.equal(resourceId)).fetchAny() != null);
-                
-                created = Boolean.TRUE;
-            }
-            else
-            {
-                var mergedIntoRecord = trx.dsl()
-                    .select(PATIENT.MERGED_INTO)
-                    .from(PATIENT)
-                    .where(PATIENT.ID.equal(resourceId))
-                    .fetchAny();
-                
-                if (mergedIntoRecord == null)
-                {
-                    created = Boolean.TRUE;
-                }
-                else if (mergedIntoRecord.value1() == null)
-                {
-                    created = Boolean.FALSE;
-                }
-                else
-                {
-                    throw new UnprocessableEntityException("Cannot update merged resource");
-                }
-            }
-
-            if (created.booleanValue())
-            {
-                patientRecord = trx.dsl().insertInto(PATIENT)
-                    .set(PATIENT.ID, resourceId)
-                    .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
-                    .set(PATIENT.IDENTIFIERS, JSONB.jsonb(identifierJson))
-                    .returningResult(PATIENT)
-                    .fetchAny().value1();
-            }
-            else
-            {
-                patientRecord = trx.dsl().update(PATIENT)
-                    .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
-                    .set(PATIENT.IDENTIFIERS, JSONB.jsonb(identifierJson))
-                    .where(PATIENT.ID.equal(resourceId))
-                    .returningResult(PATIENT)
-                    .fetchAny().value1();
-            }
-
-            return new CreateOrUpdateResult(created, patientRecord);
-        });
-
-        var resource = resourceFromRecord(result.record());
-
-        var outcome = new MethodOutcome(resource.getIdElement(), result.created());
-        outcome.setResource(resource);
-
-        return outcome;
-    }
-
-    private Patient resourceFromRecord(PatientRecord record) throws JsonProcessingException
-    {
-        var resource = new Patient();
-
-        resource.setIdElement(new IdType("Patient", record.getId()));
-
-        resource.setMeta(new Meta()
-            .setLastUpdated(Date.from(record.getLastUpdated().toInstant()))
-        );
-
-        var identifiers = objectMapper.readValue(record.getIdentifiers().data(), IdentifierRecord[].class);
-        for (var identifier : identifiers)
-        {
-            resource.addIdentifier().setSystem(identifier.system()).setValue(identifier.value());
-        }
-
-        var mergedInto = record.getMergedInto();
-        if (mergedInto != null)
-        {
-            resource.setActive(false);
-
-            resource.addLink()
-                .setOther(new Reference(new IdType("Patient", mergedInto)))
-                .setType(LinkType.REPLACEDBY);
-        }
-        else
-        {
-            resource.setActive(true);
-        }
-
-        return resource;
-    }
-
-    @JsonInclude(Include.NON_EMPTY)
-    private record IdentifierRecord(String system, String value) { }
-
-    private record CreateOrUpdateResult(Boolean created, PatientRecord record) { }
 }
---
+++ patient-index-main/src/main/java/de/tobchen/health/patientindex/main/services/PatientService.java
@@ -0,0 +1,290 @@
+package de.tobchen.health.patientindex.main.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r5.model.IdType;
+import org.hl7.fhir.r5.model.Meta;
+import org.hl7.fhir.r5.model.Patient;
+import org.hl7.fhir.r5.model.Patient.LinkType;
+import org.hl7.fhir.r5.model.Reference;
+import org.jooq.DSLContext;
+import org.jooq.JSONB;
+import org.jooq.impl.DSL;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
+import static de.tobchen.health.patientindex.main.jooq.public_.Tables.*;
+import de.tobchen.health.patientindex.main.jooq.public_.tables.records.PatientRecord;
+
+@Service
+public class PatientService
+{
+    private final DSLContext dsl;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public PatientService(DSLContext dsl)
+    {
+        this.dsl = dsl;
+    }
+
+    public MethodOutcome createOrUpdate(Patient patient)
+        throws JsonProcessingException
+    {
+        var identifierList = new ArrayList<IdentifierRecord>();
+
+        var patientIdentifiers = patient.getIdentifier();
+        if (patientIdentifiers != null)
+        {
+            for (var patientIdentifier : patientIdentifiers)
+            {
+                if (patientIdentifier != null)
+                {
+                    var system = patientIdentifier.getSystem();
+                    var value = patientIdentifier.getValue();
+                    if (system != null && value != null)
+                    {
+                        identifierList.add(new IdentifierRecord(system, value));
+                    }
+                }
+            }
+        }
+
+        var identifierJson = objectMapper.writeValueAsString(identifierList);
+
+        var transactionResult = dsl.transactionResult(trx -> {
+            var resourceIdPart = patient.getIdPart();
+
+            boolean created;
+            PatientRecord patientRecord;
+
+            if (resourceIdPart == null)
+            {
+                do
+                {
+                    resourceIdPart = UUID.randomUUID().toString();
+                }
+                while (trx.dsl().selectOne().from(PATIENT)
+                    .where(PATIENT.ID.equal(resourceIdPart)).fetchAny() != null);
+                
+                created = true;
+            }
+            else
+            {
+                var mergedIntoRecord = trx.dsl()
+                    .select(PATIENT.MERGED_INTO)
+                    .from(PATIENT)
+                    .where(PATIENT.ID.equal(resourceIdPart))
+                    .fetchAny();
+                
+                if (mergedIntoRecord == null)
+                {
+                    created = true;
+                }
+                else if (mergedIntoRecord.value1() == null)
+                {
+                    created = false;
+                }
+                else
+                {
+                    throw new UnprocessableEntityException("Cannot update merged resource");
+                }
+            }
+
+            if (created)
+            {
+                patientRecord = trx.dsl().insertInto(PATIENT)
+                    .set(PATIENT.ID, resourceIdPart)
+                    .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
+                    .set(PATIENT.IDENTIFIERS, JSONB.jsonb(identifierJson))
+                    .returningResult(PATIENT)
+                    .fetchAny().value1();
+            }
+            else
+            {
+                patientRecord = trx.dsl().update(PATIENT)
+                    .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
+                    .set(PATIENT.IDENTIFIERS, JSONB.jsonb(identifierJson))
+                    .where(PATIENT.ID.equal(resourceIdPart))
+                    .returningResult(PATIENT)
+                    .fetchAny().value1();
+            }
+
+            return new CreateOrUpdateTransactionResult(created, patientRecord);
+        });
+
+        var resource = resourceFromRecord(transactionResult.record());
+
+        var outcome = new MethodOutcome(resource.getIdElement(), transactionResult.created());
+        outcome.setResource(resource);
+
+        return outcome;
+    }
+
+    public Patient get(IIdType id)
+    {
+        Patient resource;
+
+        var record = dsl.select(PATIENT)
+            .from(PATIENT)
+            .where(PATIENT.ID.equal(id.getIdPart()))
+            .fetchAny();
+        if (record != null)
+        {
+            try {
+                resource = resourceFromRecord(record.value1());
+            } catch (JsonProcessingException e) {
+                throw new InternalErrorException("Cannot generate resource", e);
+            }
+        }
+        else
+        {
+            resource = null;
+        }
+
+        return resource;
+    }
+
+    public List<Patient> findByIdentifier(String system, String value)
+    {
+        var result = new ArrayList<Patient>();
+
+        var identifier = new IdentifierRecord(system, value);
+        String identifierJson;
+        try {
+            identifierJson = objectMapper.writeValueAsString(new IdentifierRecord[] { identifier });
+        } catch (JsonProcessingException e) {
+            throw new InternalErrorException("Cannot generate search parameter", e);
+        }
+
+        var records = dsl.select(PATIENT)
+            .from(PATIENT)
+            .where(PATIENT.IDENTIFIERS.contains(JSONB.jsonb(identifierJson)))
+            .fetch();
+        for (var record : records)
+        {
+            Patient resource;
+            try {
+                resource = resourceFromRecord(record.value1());
+            } catch (JsonProcessingException e) {
+                throw new InternalErrorException("Cannot generate resource", e);
+            }
+            result.add(resource);
+        }
+
+        return result;
+    }
+
+    public MergeResult merge(IIdType sourceId, IIdType targetId)
+    {
+        var sourceIdPart = sourceId.getIdPart();
+        var targetIdPart = targetId.getIdPart();
+
+        var transactionResult = dsl.transactionResult(trx -> {
+            var targetFetch = trx.dsl().select(PATIENT)
+                .from(PATIENT)
+                .where(PATIENT.ID.equal(targetIdPart))
+                .fetchAny();
+            if (targetFetch == null)
+            {
+                throw new InvalidRequestException("Target does not exist");
+            }
+            
+            var targetRecord = targetFetch.value1();
+            if (targetRecord.getMergedInto() != null)
+            {
+                throw new UnprocessableEntityException("Target is already merged");
+            }
+
+            var sourceFetch = trx.dsl().select(PATIENT.MERGED_INTO)
+                .from(PATIENT)
+                .where(PATIENT.ID.equal(sourceIdPart))
+                .fetchAny();
+            if (sourceFetch == null)
+            {
+                throw new InvalidRequestException("Source does not exist");
+            }
+
+            if (sourceFetch.value1() != null)
+            {
+                throw new UnprocessableEntityException("Source is already merged");
+            }
+
+            var sourceRecord = trx.dsl().update(PATIENT)
+                .set(PATIENT.LAST_UPDATED, DSL.currentOffsetDateTime())
+                .set(PATIENT.MERGED_INTO, targetIdPart)
+                .where(PATIENT.ID.equal(sourceIdPart))
+                .returningResult(PATIENT)
+                .fetchAny().value1();
+
+            return new MergeTransactionResult(sourceRecord, targetRecord);
+        });
+
+        Patient sourcePatient;
+        try {
+            sourcePatient = resourceFromRecord(transactionResult.source());
+        } catch (JsonProcessingException e) {
+            throw new InternalErrorException("Cannot generate source resource", e);
+        }
+
+        Patient targetPatient;
+        try {
+            targetPatient = resourceFromRecord(transactionResult.target());
+        } catch (JsonProcessingException e) {
+            throw new InternalErrorException("Cannot generate target resource", e);
+        }
+
+        return new MergeResult(sourcePatient, targetPatient);
+    }
+
+    private Patient resourceFromRecord(PatientRecord record) throws JsonProcessingException
+    {
+        var resource = new Patient();
+
+        resource.setIdElement(new IdType("Patient", record.getId()));
+
+        resource.setMeta(new Meta()
+            .setLastUpdated(Date.from(record.getLastUpdated().toInstant()))
+        );
+
+        var identifiers = objectMapper.readValue(record.getIdentifiers().data(), IdentifierRecord[].class);
+        for (var identifier : identifiers)
+        {
+            resource.addIdentifier().setSystem(identifier.system()).setValue(identifier.value());
+        }
+
+        var mergedInto = record.getMergedInto();
+        if (mergedInto != null)
+        {
+            resource.setActive(false);
+
+            resource.addLink()
+                .setOther(new Reference(new IdType("Patient", mergedInto)))
+                .setType(LinkType.REPLACEDBY);
+        }
+        else
+        {
+            resource.setActive(true);
+        }
+
+        return resource;
+    }
+
+    public record MergeResult(Patient source, Patient target) { }
+
+    private record IdentifierRecord(String system, String value) { }
+
+    private record CreateOrUpdateTransactionResult(boolean created, PatientRecord record) { }
+
+    private record MergeTransactionResult(PatientRecord source, PatientRecord target) { }
+}
